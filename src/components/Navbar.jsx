94e9f4ec83bf77d12d0b0735047536bd24fc9895
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-05-22 17:46:35 +0300

    Navbar design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,28 @@ import { headerLogo } from "../assets";
 
 const Navbar = () => {
   return (
-    <nav className="border-b-4 border-black">
-      <a href="/">
-        <img 
-          src={headerLogo} 
-          alt="work-order Logo" 
-          width={180}
-          height={180}
-        />
-      </a>
+    <nav>
+      <div>
+        <a href="/">
+          <img 
+            src={headerLogo} 
+            alt="work-order Logo" 
+            width={180}
+            height={180}
+          />
+        </a>
+      </div>
+      <div>
+        <p>Support</p>
+      </div>
+      <div>
+        <ul>
+          <li><a href="">Solutions</a></li>
+          <li><a href=""></a>Why Work-orders</li>
+          <li><a href=""></a></li>
+          <li><a href=""></a></li>
+        </ul>
+      </div>
     </nav>
   )
 }
