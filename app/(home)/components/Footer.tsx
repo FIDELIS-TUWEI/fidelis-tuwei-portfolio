386ae32498a9bae6ba86930ed2a0d7ff813f3e24
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-09-07 00:29:38 +0300

    Footer copyright symbol

--- app/(home)/components/Footer.tsx
+++ app/(home)/components/Footer.tsx
@@ -1,9 +1,11 @@
 import Navbar from "./Navbar";
 
 const Footer = () => {
+    const currentYear = new Date().getFullYear();
     return (
         <div className="border-t mt-10">
             <Navbar className="flex-col gap-5"/>
+            &copy; {currentYear} Fidelis Tuwei. All rights reserved.
         </div>
     )
 }
