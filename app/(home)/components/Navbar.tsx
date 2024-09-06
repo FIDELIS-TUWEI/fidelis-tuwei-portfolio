f3d7b01a290b3ba67611c1427929f749ae00cc07
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-09-07 00:22:22 +0300

    classnames prop

--- app/(home)/components/Navbar.tsx
+++ app/(home)/components/Navbar.tsx
@@ -1,8 +1,13 @@
+import { cn } from '@/lib/utils';
 import Link from 'next/link';
 import React from 'react';
 import { SiGithub, SiLinkedin, SiX } from 'react-icons/si';
 
-const Navbar = () => {
+type NavbarProps = {
+    className?: string;
+}
+
+const Navbar = ({ className }: NavbarProps) => {
     const socials = [
         {
             link: "https://linkedin.com/in/fidelis-tuwei",
@@ -22,7 +27,7 @@ const Navbar = () => {
     ];
 
     return (
-        <nav className='py-10 flex justify-between items-center'>
+        <nav className={cn('py-10 flex justify-between items-center', className)}>
             <h1 className='text2xl font-bold underline underline-offset-8 decoration-green-500 -rotate-2'>Tuwei 👩🏻‍💻</h1>
 
             <div className='flex items-center gap-5'>
