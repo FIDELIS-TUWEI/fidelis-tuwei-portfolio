d09b910faf13357638cb7feadeafe9fc3245fb4f
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-09-07 00:22:12 +0300

    Footer UI design

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,11 @@ import Navbar from './(home)/components/Navbar';
 import HeroSection from './(home)/components/HeroSection';
 import Skills from './(home)/components/Skills';
 import Projects from './(home)/components/Projects';
+import Footer from './(home)/components/Footer';
 
 const page = () => {
   return (
-    <div className='min-h-[400vh] bg-black overflow-hidden'>
+    <div className='min-h-screen bg-black overflow-hidden'>
       <div className='dark:bg-black bg-white  dark:bg-grid-white/[0.05] relative'>
         <div className='max-w-7xl mx-auto p-5'>
           <Navbar />
@@ -18,6 +19,7 @@ const page = () => {
       <div className='max-w-7xl mx-auto p-5 mt-20'>
         <Skills />
         <Projects />
+        <Footer />
       </div>
     </div>
   )
